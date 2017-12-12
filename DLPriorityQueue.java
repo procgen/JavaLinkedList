640ec37f2f879b2f45d0113f5d4dc835309a461a
JoshLorenzen <joshlorenzen49@gmail.com>
2017-12-11 21:14:52 -0800

    Documented priority queue

--- DLPriorityQueue.java
+++ DLPriorityQueue.java
@@ -3,7 +3,19 @@ public class DLPriorityQueue {
 
 	private DLinkedList list;
 	
-	public void add(Comparable<Object> element)
+	/**
+	 * Creates a new empty priority queue implemented using a doubly linked list
+	 */
+	public DLPriorityQueue()
+	{
+		list = new DLinkedList();
+	}
+	
+	/**
+	 * Adds a new element to the priority queue, placing it in the proper spot
+	 * @param element Element to store in queue
+	 */
+	public void add(Comparable element)
 	{
 		ListIterator iter = list.listIterator();
 
@@ -21,11 +33,19 @@ public class DLPriorityQueue {
 		iter.add(element);
 	}
 	
+	/**
+	 * Returns the next object in the priority queue
+	 * @return Object next object
+	 */
 	public Object peek()
 	{
 		return list.getLast();
 	}
 	
+	/**
+	 * Removes the next object in the priority queue and returns it
+	 * @return Object removed object
+	 */
 	public Object remove()
 	{
 		return list.removeLast();
